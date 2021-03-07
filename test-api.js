9e6a3b727696b5cbb55a1c7c6c224b2da13d7a1d
Erika <erika.lesantos@gmail.com>
2021-03-07 15:28:40 -0300

    Api Quote BCB

--- test-api.js
+++ test-api.js
@@ -2,4 +2,8 @@ const axios = require('axios')
 
 const url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2703-02-2021%27&$top=100&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao'
 
-axios.get(url).then(res=>console.log(res.data.value))
+
+
+axios
+.get(url)
+.then(res=>console.log(res.data.value[0].cotacaoVenda))
